a9fdfdb14129ec2da34ef29f57d9e053f89c762c
songdaochuanshu <songdaochuanshu@gmail.com>
2022-03-25 12:57:00 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./five.module.less"
+import {start, backStep} from './game';
+
+let domBtnBackStep = document.getElementById('backStep');
+//let domBtnBackStepCancel = document.getElementById('backStepCancel');
+domBtnBackStep.addEventListener('click', () => {
+    backStep();
+});
+
+//开始游戏
+start({
+    root: document.getElementById('root'),
+    onGameOver() {
+        domBtnBackStep.setAttribute('disabled', 'disabled');
+    }
+});
